dfd219657e3d11429c2ad0bebe8f1db57fa4518e
annysen <annysen95@gmail.com>
2024-01-15 03:37:57 +0600

    side cart new section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Home from "./Home";
@@ -5,27 +6,31 @@ import Nav from "./Nav";
 import SideCart from "./SideCart";
 
 function App() {
+  const [watchTime, setWatchTime] = useState("");
+
   const handleTime = (time) => {
     // console.log(time);
 
     const storedData = JSON.parse(localStorage.getItem("watchTime"));
-    const sumTime = storedData + time;
 
     if (storedData) {
+      const sumTime = storedData + time;
       localStorage.setItem("watchTime", sumTime);
+      setWatchTime(sumTime);
     } else {
       localStorage.setItem("watchTime", time);
+      setWatchTime(time);
     }
   };
   return (
     <div className="mx-7 md:mx-12">
       <Nav />
       <div className=" grid grid-cols-3 my-5 gap-5">
-        <span className="col-span-2">
+        <span className="col-span-1 md:col-span-2">
           <Home handleTime={handleTime} />
         </span>
-        <span>
-          <SideCart />
+        <span className="">
+          <SideCart watchTime={watchTime} />
         </span>
       </div>
       <Footer />
--- src/SideCart.jsx
+++ src/SideCart.jsx
@@ -1,4 +1,20 @@
-const SideCart = () => {
+import { useEffect, useState } from "react";
+
+const SideCart = ({ watchTime }) => {
+  const [time, setTime] = useState(watchTime);
+
+  useEffect(() => {
+    const getTime = JSON.parse(localStorage.getItem("watchTime"));
+    setTime(getTime);
+  }, [watchTime]);
+
+  const clearTime = () => {
+    localStorage.clear();
+
+    const loadTime = JSON.parse(localStorage.getItem("watchTime"));
+    console.log(loadTime);
+    setTime(0);
+  };
   return (
     <div className=" px-5">
       <div className="card w-auto bg-base-100 shadow-xl">
@@ -7,13 +23,28 @@ const SideCart = () => {
           <div className="stats shadow w-auto">
             <div className="stat">
               <div className="stat-title">Total Watching Time</div>
-              <div className="stat-value">89,400</div>
-              <div className="stat-desc">21% more than last month</div>
+              <div className="stat-value">{time}</div>
+              <div className="stat-title">Add Break Time</div>
+              <div className="card-actions">
+                <button className="btn btn-secondary">5 min</button>
+                <button className="btn btn-accent">10 min</button>
+                <button className="btn btn-info">15 min</button>
+              </div>
+              <input
+                disabled
+                value={time}
+                type="text"
+                placeholder="Break Time"
+                className="input input-bordered w-full max-w-xs mt-5"
+              />
             </div>
           </div>
 
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Reset</button>
+          <div className="card-actions justify-end mt-3">
+            <button onClick={clearTime} className="btn btn-warning">
+              Reset
+            </button>
+            <button className="btn btn-primary">Compleat</button>
           </div>
         </div>
       </div>
